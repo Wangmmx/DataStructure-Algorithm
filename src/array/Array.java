b7b9bff705bcb2cbf4f550e4731630388aca396c
Wangmmx <437299476@qq.com>
2018-11-13 16:09:55 +0800

    feat(array): add method

--- src/array/Array.java
+++ src/array/Array.java
@@ -45,5 +45,38 @@ public class Array {
     public boolean isEmpty() {
         return size == 0;
     }
-    
+
+    /**
+     * 在指定index处添加一个元素e
+     * @param index
+     * @param e
+     */
+    public void add(int index, int e) {
+        if (size == data.length)
+            throw new IllegalArgumentException("Add failed, Array is full");
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed, index is out of range");
+        for(int i = size; i >= 0; i--) {
+            data[i+1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    /**
+     * 末尾添加元素
+     * @param e
+     */
+    public void addLast(int e) {
+        add(size, e);
+    }
+
+    /**
+     * 第一个位置添加元素
+     * @param e
+     */
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
 }
