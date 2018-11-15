7ef3a8cba98adefc007a44bfc4de54eb9e2020dc
Wangmmx <437299476@qq.com>
2018-11-15 17:53:12 +0800

    feat(linkedlist): add add method

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -1,6 +1,9 @@
 package linkedList;
 
 public class LinkedList<E> {
+    /**
+     * 定义内部类 Node
+     */
     private class Node {
         public E e;
         public Node next;
@@ -24,5 +27,58 @@ public class LinkedList<E> {
         public String toString() {
             return e.toString();
         }
+    }
+
+    private Node head;
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 在头部添加元素
+     * @param e
+     */
+    public void addFirst(E e) {
+       Node node = new Node();
+       node.next = head;
+       head = node;
+       size++;
+    }
+
+    /**
+     * 在末尾添加元素
+     * @param e
+     */
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    /**
+     * 在指定位置添加元素
+     * @param index
+     * @param e
+     */
+    public void add(int index, E e) {
+
+        if (index < 0 || index > size) throw new IllegalArgumentException("Add failed, index out of range");
+        if (index == 0) addFirst(e);
+        Node prev = head;
+        for (int i = 0; i <index -1; i++) {
+            prev = prev.next;
+        }
+        Node node = new Node();
+        node.next = prev.next;
+        prev.next = node;
+        size++;
     }
 }
