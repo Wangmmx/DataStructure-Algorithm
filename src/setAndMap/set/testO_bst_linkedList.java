3323f3b5298def8274a7e2bd1e45be30aef9f2db
Wangmmx <437299476@qq.com>
2018-11-20 16:35:13 +0800

    feat(setAndMap): add test to linkedlist and bst

---
+++ src/setAndMap/set/testO_bst_linkedList.java
@@ -0,0 +1,24 @@
+package setAndMap.set;
+
+import java.util.Random;
+
+public class testO_bst_linkedList {
+    private static double testSet(Set<Integer> set, int opCount) {
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            set.add(random.nextInt(Integer.MAX_VALUE));
+        }
+        long endTime = System.nanoTime();
+        return (endTime-startTime)/1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        int opCount = 100000;
+        LinkedListSet<Integer> listSet = new LinkedListSet<>();
+        System.out.println("LinkedListSet: " + testSet(listSet, opCount));
+        BSTSet<Integer> bstSet = new BSTSet<>();
+        System.out.println("BSTSet: " + testSet(bstSet, opCount));
+    }
+
+}
